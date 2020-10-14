a413c5b39f562c3deed1912c49cedfbfa5919d16
UyeonS <dusdl14@gmail.com>
2020-10-14 21:45:40 +0900

    Static Timer 객체 추가

---
+++ Flo/TimerManger/TimerManger.swift
@@ -0,0 +1,16 @@
+//
+//  TimerManger.swift
+//  Flo
+//
+//  Created by 신의연 on 2020/10/12.
+//
+
+import Foundation
+import UIKit
+class TimerManger {
+    
+    static let shared = TimerManger()
+    
+    var timer: Timer!
+
+}
