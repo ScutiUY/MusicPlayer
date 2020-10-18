b0a4517ffed5cdbf00d419a0c5b6677f58dec49b
UyeonS <dusdl14@gmail.com>
2020-10-18 22:43:15 +0900

    static Timer update

--- Flo/TimerManger/TimerManger.swift
+++ Flo/TimerManger/TimerManger.swift
@@ -7,10 +7,33 @@
 
 import Foundation
 import UIKit
+
 class TimerManger {
     
     static let shared = TimerManger()
     
     var timer: Timer!
-
+    
+    func makeAndFireTimer() {
+        TimerManger.shared.timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { [unowned self] (timer: Timer) in
+            
+            if ProgressBarAndTimeLabelStackView.shared.progressSlider.isTracking { return }
+            
+            NotificationCenter.default.post(name: .getCurrentTime, object: nil, userInfo: nil)
+            
+            ProgressBarAndTimeLabelStackView.shared.progressSlider.value = Float(SoundManager.shared.player.currentTime)
+            //self.updateTimeLabelText(time: SoundManager.shared.player.currentTime)
+            //updateLyricsTextView()
+        })
+        TimerManger.shared.timer.fire()
+        //각자 tableView에 업데이트 가사 노티 보내기
+    }
+    
+    func invalidateTimer() {
+        TimerManger.shared.timer?.invalidate()
+        TimerManger.shared.timer = nil
+    }
+}
+extension Notification.Name {
+    static let getCurrentTime = Notification.Name("getCurrentTime")
 }
