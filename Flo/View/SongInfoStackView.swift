ff8a75e2982b1481b2b37058026e392fd95df8e3
UyeonS <dusdl14@gmail.com>
2020-10-19 21:19:04 +0900

    add MarqueeLabel

--- Flo/View/SongInfoStackView.swift
+++ Flo/View/SongInfoStackView.swift
@@ -7,11 +7,18 @@
 
 import Foundation
 import UIKit
-
+import MarqueeLabel
 class SongInfoStackView: UIStackView {
     
-    lazy var songTitle = PlayerButton(fontSize: 22)
+    //lazy var songTitle = PlayerButton(fontSize: 22)
     lazy var albumTitle = PlayerButton(fontSize: 18)
+    lazy var songTitle: MarqueeLabel = {
+        var label = MarqueeLabel(frame: .zero, duration: 8, fadeLength: 10)
+        label.text = playList[0].title
+        label.font = label.font.withSize(22)
+        label.textColor = .white
+        return label
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: .zero)
@@ -21,13 +28,13 @@ class SongInfoStackView: UIStackView {
         self.spacing = -10
         self.addArrangedSubview(songTitle)
         self.addArrangedSubview(albumTitle)
+        
         fetchMusicInfo()
     }
     required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     private func fetchMusicInfo() {
-        songTitle.setTitle(playList[0].title, for: .normal)
         albumTitle.setTitle(playList[0].albumTitle, for: .normal)
     }
 }
